5a0348a633d67d1f4896d6987c0a6a81bb723562
Gagandeep <gagandeep.wadhwa@servicenow.com>
2018-07-01 00:10:01 +0530

    adding container to code

--- app.js
+++ app.js
@@ -3,7 +3,28 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const request = require('request');
+const request = require('request'),
+express = require('express'),
+body_parser = require('body-parser'),
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.use(body_parser.json());
+app.use(express.static('public'));
+
+
+app.get('/testDummy', (req, res) => {
+    let body = req.query;
+    let response = {
+        "text": `Great, i got your message happy to see you back`
+    };
+
+    res
+        .status(200)
+        .send('Please close this window to return to the conversation thread.');
+    
+});
 
 function callSendAPI( response) {
     // Construct the message body
